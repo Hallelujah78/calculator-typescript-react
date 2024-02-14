1552ab4e09142392fc7d34483bff376e62323d43
Hallelujah78 <gavan.browne@gmail.com>
2024-02-14 22:34:47 +0000

    Updated eslint config.
    Eslint config updated to that recommended for production app."

--- src/App.tsx
+++ src/App.tsx
@@ -13,3 +13,4 @@ export default App;
 // only one decimal place per number allowed
 // numbers are separated by operators
 // a number can only be 15 digits long
+// the maximum number of digits after the decimal is 10
